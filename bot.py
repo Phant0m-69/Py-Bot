73247bc8727069f6916ab497bdb52f046c280817
Phant0m <72923658+Phant0m-69@users.noreply.github.com>
2020-10-17 15:11:17 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,5 +1,6 @@
 import discord
 import os
+npm i profanities
 
 client = discord.Client()
 
@@ -12,3 +13,13 @@ async def on_message(message):
         await message.channel.send("Hi! I am Py-Bot")
 
 client.run(os.environ['TOKEN'])
+
+#anti-profanity filter
+var profanities = require('profanities',)
+
+//profanity
+for(x in profanities)
+  if(message.content.toUpperCase() == profanities(x).toUpperCase())
+     message.channel.send("Hey, Don't say that!")
+     message.delete()
+     return;
